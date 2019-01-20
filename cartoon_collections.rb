d0fcf6566cab85157ab5a9166dcaa3d768847a5b
yong liang <yongliang24@gmail.com>
2019-01-20 18:06:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,11 @@ x.capitalize + "!"
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(str)# code an argument here
   # Your code here
+  str.any? do |x|
+    x.length>4
+  end
 end
 
 def find_the_cheese# code an argument here
